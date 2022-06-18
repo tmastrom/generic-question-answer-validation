2ca5cf505d8e8fcad728bfb453943403a007d4e9
tmastro <thomas.mastromonaco@gmail.com>
2022-06-18 13:17:19 -0700

    perform nlp similarity comparison on user input

--- server.py
+++ server.py
@@ -1,28 +1,45 @@
-from flask import Flask, render_template, request#, session
-# from flask.ext.session import Session
+from flask import Flask, render_template, request, session
+
+from google import search_google
+from scraper import wiki_scraper
+from nlp import semantic_transform, checkAnswer
 
 app = Flask(__name__)
-# SESSION_TYPE = 'redis'
-# app.config.from_object(__name__)
-# Session(app)
+# Details on the Secret Key: https://flask.palletsprojects.com/en/1.1.x/config/#SECRET_KEY
+# NOTE: The secret key is used to cryptographically-sign the cookies used for storing
+#       the session data.
+app.secret_key = 'BAD_SECRET_KEY'
 
 @app.route("/")
 def question():
     return render_template('question.html')
 
-@app.route("/answer", methods = ['POST', 'GET'])
+@app.route("/result", methods = ['POST', 'GET'])
 def answer():
     if request.method == 'POST':
         question = request.form.get('question')
-        # pass the question entered by the user 
-        # session['my_question'] = question
-        return render_template('answer.html', question = question)
-
-@app.route('/result', methods = ['POST', 'GET'])
-def result():
-    if request.method == 'POST':
         answer = request.form.get('answer')
-        question = "Who am I?"
-        percent = '99'
-        return render_template('result.html', question = question, answer = answer, percent = percent)
+        
+        session['question'] = question
+        session['answer'] = answer
 
+        # search google for a wiki page on the question topic 
+        # TODO: error handling 
+        wikiUrl = search_google(question)
+        
+        # Scrape text from wiki page and return filtered text
+        corpus = wiki_scraper(wikiUrl)
+
+        # format the corpus for nlp training
+        dictionary, lsi, index = semantic_transform(corpus)
+
+        # check the answer agains the semantic vectorization and determine if correct
+        percent = checkAnswer(dictionary, lsi, index, answer)
+
+        return render_template('result.html', percent = percent)
+
+
+
+        
+        
+        
